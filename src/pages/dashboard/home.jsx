84a5a1282f381b2b1b1c06b00062fc7f9ba8e9ca
ravindu <ravindusachinthalakshan@gmail.com>
2024-06-06 15:42:46 +0530

    updated price hike chart settings

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -51,10 +51,7 @@ const priceHikeChart = {
   type: "line",
   height: 220,
   series: [
-    {
-      name: "Purchases",
-      data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
-    },
+    
   ],
   options: {
     ...chartsConfig,
@@ -66,20 +63,7 @@ const priceHikeChart = {
     markers: {
       size: 5,
     },
-    xaxis: {
-      ...chartsConfig.xaxis,
-      categories: [
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-      ],
-    },
+    
   },
 };
 
